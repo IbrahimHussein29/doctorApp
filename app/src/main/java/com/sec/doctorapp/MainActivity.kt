427c2a1c30923f74030ace368e3a0d2fbbc42bb0
Ibrahim Hussein <ibrahim.hussein.me@gmail.com>
2023-09-13 20:38:02 +0300

    doctorApp

--- app/src/main/java/com/sec/doctorapp/MainActivity.kt
+++ app/src/main/java/com/sec/doctorapp/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.sec.doctorapp
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.window.SplashScreen
 import android.window.SplashScreenView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.google.android.filament.Box
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.sec.doctorapp.auth.AuthActivity
-import java.lang.reflect.Modifier
+import com.sec.doctorapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
-        finish()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+            navController=Navigation.findNavController(this,R.id.fragment_container_view_main)
+            setupWithNavController(binding.bottomNavView, navController)
 
-        startActivity(Intent(this, AuthActivity::class.java))
+//        finish()
+
+//        startActivity(Intent(this, AuthActivity::class.java))
 
 
 
